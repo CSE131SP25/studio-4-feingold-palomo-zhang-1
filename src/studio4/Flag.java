5e40797976d5feff0ea73a061371f5b191b39c08
elizabeth.feingold <elizabeth.feingold@localhost>
2025-02-19 14:15:14 -0600

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,34 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		 
+		StdDraw.setPenColor(2, 48, 32);
+		StdDraw.filledRectangle(.175, .5, .075, .25);
+		StdDraw.setPenColor(2, 48, 32);
+		StdDraw.filledRectangle(.825, .5, .075, .25);
+		StdDraw.setPenColor(173, 216, 230);
+		StdDraw.filledRectangle(.5, .5, .25, .25);
+		
+		StdDraw.setPenColor(255, 255, 197);
+		StdDraw.filledCircle(.5, .5, .1);
+		
+		StdDraw.setPenColor(2, 48, 32);
+		StdDraw.filledCircle(.3, .3, .02);
+		StdDraw.setPenColor(2, 48, 32);
+		StdDraw.filledCircle(.7, .3, .02);
+		StdDraw.setPenColor(2, 48, 32);
+		StdDraw.filledCircle(.3, .7, .02);
+		StdDraw.setPenColor(2, 48, 32);
+		StdDraw.filledCircle(.7, .7, .02);
+		
+		StdDraw.setPenColor(255, 255, 197);
+		StdDraw.line(.5, .4, .5, .3);
+		StdDraw.setPenColor(255, 255, 197);
+		StdDraw.line(.5, .6, .5, .7);
+		StdDraw.setPenColor(255, 255, 197);
+		StdDraw.line(.4, .5, .3, .5);
+		StdDraw.setPenColor(255, 255, 197);
+		StdDraw.line(.6, .5, .7, .5);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,20 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int color1 = in.nextInt();
+		int color2 = in.nextInt();
+		int color3 = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double width = in.nextDouble();
+		double height = in.nextDouble();
+		
+		if (shape == "rectangle") {
+			StdDraw.setPenColor(color1, color2, color3);
+			StdDraw.rectangle(x, y, width, height);
+		}
 		
 	}
 }
